3e5738c1143aefeb594d36ab7164108487bd36db
Subesta Romany <subestaromany@gmail.com>
2025-06-02 19:26:43 +0300

    add on admin

--- donations/admin.py
+++ donations/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Donation
 
-# Register your models here.
+@admin.register(Donation)
+class DonationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'project', 'amount', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('user__email', 'project__title')
